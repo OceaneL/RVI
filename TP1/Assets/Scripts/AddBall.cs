4fec682239982b26ca36d9a5bc6fffa39fe3f92e
Legrand Oceane <Legrand Oceane>
2017-09-12 15:18:01 +0200

    RVI après les deux premieres sceance de tps

---
+++ TP1/Assets/Scripts/AddBall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddBall : MonoBehaviour {
+
+	public Plane groundPlane;
+	public GameObject ball;
+	public GameObject planeloc;
+
+	// Use this for initialization
+	void Start() {
+		Debug.Log ("aaaa");
+		groundPlane = new Plane (planeloc.transform.forward, planeloc.transform.position);	
+		Debug.Log ("hsfdhzfehé");
+		//Debug.Log(groundPlane);
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			float rayDistance;
+			if (groundPlane.Raycast (ray, out rayDistance)) {
+				Instantiate(ball, ray.GetPoint (rayDistance), Quaternion.identity);
+			}
+
+		}
+	}
+
+}
---
+++ TP1/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+	public GameObject cible;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(cible.transform);
+		transform.RotateAround(cible.transform.position, Vector3.up, 20 * Time.deltaTime);
+	}
+}
