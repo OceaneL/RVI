a60f62027d858a07c4443b4bdf1c60e495d84f47
OceaneL <legrand@b02p2.FIL>
2017-09-28 16:55:45 +0200

    opartie 2.2 fonctionelle

--- TP1/Assets/Scripts/InterctionSouris.cs
+++ TP1/Assets/Scripts/InterctionSouris.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class InterctionSouris : MonoBehaviour {
 
 	public Text gt;
+	private bool displayed = false;
+	private GameObject selectedObject = null;
 
 	// Use this for initialization
 	void Start () {
@@ -16,20 +18,48 @@ public class InterctionSouris : MonoBehaviour {
 	void Update () {
         if (Input.GetMouseButtonDown(0))
         {
+			
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo;
             if (Physics.Raycast(ray, out hitInfo))
             {
                 Debug.Log(hitInfo.transform.gameObject.name);
-                if (hitInfo.transform.gameObject.GetComponent(typeof(GUIText)) == null)
+				if (selectedObject != hitInfo.transform.gameObject) {
+					displayed = true;
+					selectedObject = hitInfo.transform.gameObject;
+				} else {
+					displayed = false;
+					selectedObject = null;
+				}
+				
+                /*if (hitInfo.transform.gameObject.GetComponent(typeof(GUIText)) == null)
                 {
                     //GUIText gt = hitInfo.transform.gameObject.AddComponent(typeof(GUIText)) as GUIText;
                     gt.text = hitInfo.transform.gameObject.name;
 					//gt.rectTransform.position = hitInfo.point;
 					gt.alignment = TextAnchor.MiddleCenter;
-                }
+                }*/
+
+				/*
+				var horizontalSpeed = 2.0;
+
+				function OnMouseDrag()
+				{
+					var h = horizontalSpeed * Input.GetAxis ("Mouse X");
+					transform.Translate(h, 0, 0);
+				}
+				*/
             }
 
         }
+	}
+
+	void OnGUI(){
+		if (displayed) {
+			Vector3 pos = selectedObject.transform.position;
+			Vector3 pos2d = Camera.main.WorldToScreenPoint (pos);
+			Rect posRect = new Rect (pos2d.x, pos2d.y, 100, 100);
+			GUI.Label(posRect, selectedObject.name + "\n"+ pos); 
+		}
 	}
 }
