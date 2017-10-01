f92dd09b4e1829791aa5355aaace24b5b4d29fe7
OceaneL <oceane.legrand@etudiant.univ-lille1.fr>
2017-10-01 22:09:23 +0200

    fin tp6 partie 2

--- TP1/Assets/Scripts/InterctionSouris.cs
+++ TP1/Assets/Scripts/InterctionSouris.cs
@@ -8,47 +8,91 @@ public class InterctionSouris : MonoBehaviour {
 	public Text gt;
 	private bool displayed = false;
 	private GameObject selectedObject = null;
+    private float lastH;
+    private float lastV;
+    private bool objectIsSelected = false;
+    private bool objectIsSelected2 = false;
+    private GameObject selectedObject2 = null;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
+
+
+
+				
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButtonDown(0))
         {
-			
+
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo;
             if (Physics.Raycast(ray, out hitInfo))
             {
-                Debug.Log(hitInfo.transform.gameObject.name);
-				if (selectedObject != hitInfo.transform.gameObject) {
-					displayed = true;
-					selectedObject = hitInfo.transform.gameObject;
-				} else {
-					displayed = false;
-					selectedObject = null;
-				}
-				
-                /*if (hitInfo.transform.gameObject.GetComponent(typeof(GUIText)) == null)
+
+                if (selectedObject != hitInfo.transform.gameObject || displayed == false)
                 {
-                    //GUIText gt = hitInfo.transform.gameObject.AddComponent(typeof(GUIText)) as GUIText;
-                    gt.text = hitInfo.transform.gameObject.name;
-					//gt.rectTransform.position = hitInfo.point;
-					gt.alignment = TextAnchor.MiddleCenter;
-                }*/
+                    displayed = true;
+                    selectedObject = hitInfo.transform.gameObject;
+                }
+                else
+                {
+                    displayed = false;
+                    //selectedObject = null;
+                }
 
-				/*
-				var horizontalSpeed = 2.0;
+                objectIsSelected = true;
+            }
 
-				function OnMouseDrag()
-				{
-					var h = horizontalSpeed * Input.GetAxis ("Mouse X");
-					transform.Translate(h, 0, 0);
-				}
-				*/
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            objectIsSelected = false;
+        }
+       // Debug.Log(objectIsSelected);
+        if (Input.GetMouseButton(0) && objectIsSelected)
+        {
+            Plane groundPlane = new Plane(Vector3.up, selectedObject.transform.position);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float rayDistance;
+            if (groundPlane.Raycast(ray, out rayDistance)) {
+                selectedObject.transform.position = ray.GetPoint(rayDistance);
+            }
+            
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+
+                selectedObject2 = hitInfo.transform.gameObject;
+                objectIsSelected2 = true;
+
+            }
+
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            objectIsSelected = true;
+        }
+
+
+        if (Input.GetMouseButton(1) && objectIsSelected2)
+        {
+            Plane groundPlane = new Plane(Camera.main.transform.forward, selectedObject2.transform.position);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float rayDistance;
+            if (groundPlane.Raycast(ray, out rayDistance))
+            {
+                selectedObject2.transform.position = ray.GetPoint(rayDistance);
             }
 
         }
@@ -58,8 +102,10 @@ public class InterctionSouris : MonoBehaviour {
 		if (displayed) {
 			Vector3 pos = selectedObject.transform.position;
 			Vector3 pos2d = Camera.main.WorldToScreenPoint (pos);
-			Rect posRect = new Rect (pos2d.x, pos2d.y, 100, 100);
+			Rect posRect = new Rect (pos2d.x, Screen.height - pos2d.y, 100, 100);
 			GUI.Label(posRect, selectedObject.name + "\n"+ pos); 
 		}
 	}
+
+    
 }
